f1d47a882854752b9cf400b0c2ac8e388ff176d2
lucasaking <70596715+lucasaking@users.noreply.github.com>
2020-10-20 14:33:34 -0400

    first commit

---
+++ models.py
@@ -0,0 +1,68 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+
+bcrypt = Bcrypt()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    """Site user."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    first_name = db.Column(db.String(30),
+                         nullable=False)
+
+    last_name = db.Column(db.String(30),
+                         nullable=False)
+
+    email = db.Column(db.String(50),
+                         nullable=False)
+
+    username = db.Column(db.String(20),
+                         nullable=False,
+                         unique=True)
+
+    password = db.Column(db.Text,
+                         nullable=False)
+
+    # start_register
+    @classmethod
+    def register(cls, username, pwd):
+        """Register user w/hashed password & return user."""
+
+        hashed = bcrypt.generate_password_hash(pwd).decode('utf8')
+
+        # return instance of user w/username and hashed pwd
+        return cls(username=username, password=hashed)
+
+    # end_register
+
+    # start_authenticate
+    @classmethod
+    def authenticate(cls, username, pwd):
+        """Validate that user exists & password is correct.
+
+        Return user if valid; else return False.
+        """
+
+        u = User.query.filter_by(username=username).first()
+
+        if u and bcrypt.check_password_hash(u.password, pwd):
+            # return user instance
+            return u
+        else:
+            return False
+    # end_authenticate
